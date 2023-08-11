11e9f2af8f4ba2a6e4aab374926fc4cace002f14
Krzysztof Wojciechowski <49921081+Kotochleb@users.noreply.github.com>
2023-08-11 09:42:04 +0200

    Docker and Docker Compose for 1.1.0 software stack (#28)
    * Move config files to new directory
    
    * Remove psutil installation
    
    * Remove privileged from panther_ros
    
    * Allow for mutable env variables (#26)
    
    * Allow for mutable env variables
    
    * Add usd to devices | reformat command
    
    * Bring back more exact path to iio devices
    
    * Update gamepad docker version (#27)
    
    * Fix build not being dispatched (#25)
    
    * update gamepad docker version
    
    ---------
    
    Co-authored-by: Krzysztof Wojciechowski <49921081+Kotochleb@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Dawid Kmak <73443304+KmakD@users.noreply.github.com>

--- ros_entrypoint.sh
+++ ros_entrypoint.sh
@@ -5,4 +5,8 @@ set -e
 source "/opt/ros/$ROS_DISTRO/setup.bash"
 source "/ros_ws/devel/setup.bash"
 
+# source eeprom environemnt variables
+set -a
+source "/run/husarion/panther_config.env"
+
 exec "$@"
