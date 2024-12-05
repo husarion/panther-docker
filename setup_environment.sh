a6fcd38bc62c3f4eb3ab2708db880ade2e4b6567
kmakd <kmakd197@gmail.com>
2024-12-05 13:19:25 +0000

    fix setup and compose

--- setup_environment.sh
+++ setup_environment.sh
@@ -5,7 +5,7 @@ set -e
 source /run/husarion/robot_config.env
 
 # check if /config directory is empty except it may contain common directory inside
-if [ "$(ls -A /config | grep -v common)" ]; then
+if [ ! "$(ls -A /config | grep -v common)" ]; then
     echo "Config directory is empty, copying files."
     update_config_directory
 fi
